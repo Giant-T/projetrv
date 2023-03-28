18cf6540718b14a39ff183147e89d26d456ab295
William Boudreault <boudreaultwilliam@gmail.com>
2023-03-28 16:18:51 -0400

    Ajout de l'animation

--- Assets/Scripts/ControleurAnimMains.cs
+++ Assets/Scripts/ControleurAnimMains.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// Source: https://cegepvicto.sharepoint.com/sites/Section_27335/Supports%20de%20cours/Forms/AllItems.aspx?id=%2Fsites%2FSection%5F27335%2FSupports%20de%20cours%2FAssets%2FControleurAnimMains%2Ecs&parent=%2Fsites%2FSection%5F27335%2FSupports%20de%20cours%2FAssets&p=true&ga=1
 public class ControleurAnimMains : MonoBehaviour
 {
     [SerializeField] private InputActionProperty actionTrigger;
